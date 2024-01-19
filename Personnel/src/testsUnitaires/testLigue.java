070163a086a2024bab910f4af1dda9e5a63a687e
laetitiamichel <laetitia.michel@devoteam.com>
2024-01-19 16:26:14 +0100

    mdc

--- Personnel/src/testsUnitaires/testLigue.java
+++ Personnel/src/testsUnitaires/testLigue.java
@@ -1,6 +1,9 @@
 package testsUnitaires;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 
 import personnel.*;
@@ -32,7 +35,7 @@ class testLigue
 			Ligue ligue = gestionPersonnel.addLigue("Fléchettes");// création d'une ligue
 
 	        // Créer une instance de la classe à tester
-		 	Employe employe = ligue.addEmploye("Michel", null, null, null, null,null);
+		 	Employe employe = ligue.addEmploye("Michel", "L", null, null, null,null);
 
 	        // Appeler le getter et vérifier si la valeur renvoyée est correcte
 	        assertEquals("Michel", employe.getNom());
@@ -44,7 +47,7 @@ class testLigue
 		 	Ligue ligue = gestionPersonnel.addLigue("Fléchettes");// création d'une ligue   
 		 	
 		 	// Créer une instance de la classe à tester
-		 	Employe employe = ligue.addEmploye(null,"Laetitia", null, null, null,null);
+		 	Employe employe = ligue.addEmploye("Michel","Laetitia", null, null, null,null);
 
 	        // Appeler le getter et vérifier si la valeur renvoyée est correcte
 	        assertEquals("Laetitia", employe.getPrenom());
@@ -64,27 +67,27 @@ class testLigue
 	   }
 	 
 	 @Test 
-	 public void getDateArrivee() throws SauvegardeImpossible 
+	 public void getDateDepart() throws SauvegardeImpossible 
 	 {
 		 	Ligue ligue = gestionPersonnel.addLigue("Fléchettes");// création d'une ligue   
-		 	
+		 	LocalDate DateDepart = LocalDate.parse("1/01/2001");
 		 	// Créer une instance de la classe à tester
-		 	Employe employe = ligue.addEmploye(null, null,null, "01/01/2001", null);
+		 	Employe employe = ligue.addEmploye(null, null,null, null, null,DateDepart);
 		 	
 	        // Appeler le getter et vérifier si la valeur renvoyée est correcte
-	        assertEquals("01/01/2001", employe.getDateArrivee());
+	        assertEquals("1/01/2001", employe.getDateDepart());
 	   }
 	 
 	 @Test 
 	 public void getDateArrivee() throws SauvegardeImpossible 
 	 {
 		 Ligue ligue = gestionPersonnel.addLigue("Fléchettes");// création d'une ligue   
-		 	
+		 	LocalDate dateArrivee = LocalDate.parse("1/01/2001");
 		 	// Créer une instance de la classe à tester
-		 	Employe employe = ligue.addEmploye(null, null,null, null, "01/01/2011");
-
+		 	Employe employe = ligue.addEmploye(null, null,null, null,dateArrivee, null);
+		 	
 	        // Appeler le getter et vérifier si la valeur renvoyée est correcte
-	        assertEquals("01/01/2011", employe.getDateArrivee());
+	        assertEquals("1/01/2001", employe.getDateArrivee());
 	   }
 	
 }
